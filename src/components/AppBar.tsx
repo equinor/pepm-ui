2e3877765a586d0e1f275c72ac5da5584fcdd1bb
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-03-28 14:30:42 +0200

    chore: style TopBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,27 +1,66 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { Icon, TopBar } from '@equinor/eds-core-react'
+import { Icon, Tabs, TopBar } from '@equinor/eds-core-react'
 import {
   account_circle as accountCircle,
   notifications,
 } from '@equinor/eds-icons'
+import styled from 'styled-components'
+import { theme } from '../theme'
+
+const StyledTopBar = styled(TopBar)`
+  min-height: 72px;
+  height: 100%;
+  padding-bottom: 0px;
+  padding-top: 0px;
+`
+
+const StyledTabs = styled(Tabs)`
+  display: flex;
+  height: inherit;
+  justify-content: flex-end;
+
+  > * {
+    display: flex;
+    height: inherit;
+    align-items: center;
+
+    > * {
+      height: inherit;
+    }
+  }
+`
+
+const Icons = styled.div`
+  display: flex;
+  padding-left: 1.5em;
+  gap: 1.5em;
+  color: ${theme.light.primary.resting};
+`
+
+const NavigationTabs = () => {
+  return (
+    <StyledTabs>
+      <Tabs.List>
+        <Tabs.Tab>Add model</Tabs.Tab>
+        <Tabs.Tab>Browse</Tabs.Tab>
+        <Tabs.Tab>API</Tabs.Tab>
+        <Tabs.Tab>About</Tabs.Tab>
+      </Tabs.List>
+    </StyledTabs>
+  )
+}
 
 const AppBar = ({ title }: { title: string }) => {
   return (
-    <TopBar>
+    <StyledTopBar>
       <TopBar.Header>{title}</TopBar.Header>
-      <TopBar.CustomContent>
-        <nav>
-          <a href="#">Add model</a>
-          <a href="#">Browse</a>
-          <a href="#">API</a>
-          <a href="#">About</a>
-        </nav>
-      </TopBar.CustomContent>
+      <NavigationTabs />
       <TopBar.Actions>
-        <Icon name="notifications" data={notifications} />
-        <Icon name="accountCircle" data={accountCircle} />
+        <Icons>
+          <Icon name="notifications" data={notifications} />
+          <Icon name="accountCircle" data={accountCircle} />
+        </Icons>
       </TopBar.Actions>
-    </TopBar>
+    </StyledTopBar>
   )
 }
 
