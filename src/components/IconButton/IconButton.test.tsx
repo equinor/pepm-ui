63d5f9cfa6a014d5bfcd8feeb4cc2fa87c22d72a
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-04-04 15:35:21 +0200

    test: IconButton

---
+++ src/components/IconButton/IconButton.test.tsx
@@ -0,0 +1,13 @@
+import { notifications } from '@equinor/eds-icons'
+import { fireEvent, render, screen } from '@testing-library/react'
+import IconButton from './IconButton'
+
+test('calls onClick callback when clicked', () => {
+  const handleClick = jest.fn()
+  render(
+    <IconButton title={'Title'} icon={notifications} onClick={handleClick} />
+  )
+  const iconButton = screen.getByRole('button')
+  fireEvent.click(iconButton)
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
