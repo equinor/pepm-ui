6159ebdb133cc73cd6d4c52c27384ff088ae651c
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-04-04 16:22:25 +0200

    chore: add spacings based on equinor tokens

---
+++ src/tokens/spacings.ts
@@ -0,0 +1,15 @@
+import { tokens } from '@equinor/eds-tokens'
+
+const comfortable = tokens.spacings.comfortable
+
+export const spacings = {
+  XX_SMALL: comfortable.xx_small /* 2px */,
+  X_SMALL: comfortable.x_small /* 4px */,
+  SMALL: comfortable.small /* 8px */,
+  MEDIUM_SMALL: comfortable.medium_small /* 12px */,
+  MEDIUM: comfortable.medium /* 16px */,
+  LARGE: comfortable.large /* 24px */,
+  X_LARGE: comfortable.x_large /* 32px */,
+  XX_LARGE: comfortable.xx_large /* 40px */,
+  XXX_LARGE: comfortable.xxx_large /* 48px */,
+}
