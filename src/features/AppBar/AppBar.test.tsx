154918ae30e32fa5c80e2c7034e63558fe026816
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-04-04 21:29:15 +0200

    test: separate tests for AppBar and MenuIcon

--- src/features/AppBar/AppBar.test.tsx
+++ src/features/AppBar/AppBar.test.tsx
@@ -1,25 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import AppBar from './AppBar'
 
-test('renders navigation tabs', () => {
+test('has title', () => {
   render(<AppBar title={'Title'} />)
-  expect(screen.getAllByRole('tab')).toBeDefined()
+  const title = screen.getByText('Title')
+  expect(title).toBeDefined()
 })
 
-test('opens a menu when the user icon is clicked', () => {
+test('has navigation tabs', () => {
   render(<AppBar title={'Title'} />)
-  const userIconButton = screen.getByRole('button', { name: 'UserInfo' })
-  const menu = screen.getByText('Logged in')
+  const tabs = screen.getAllByRole('tab')
+  expect(tabs).toBeDefined()
+})
 
-  fireEvent.click(userIconButton)
-  expect(menu).toBeVisible()
-
-  // Hide the menu again on another click
-  fireEvent.click(userIconButton)
-  expect(menu).not.toBeVisible()
-
-  // TODO: Hide menu on click outside itself
-  // fireEvent.click(userIconButton)
-  fireEvent.click(window)
-  expect(menu).not.toBeVisible()
+test('has action icons', () => {
+  render(<AppBar title={'Title'} />)
+  const actionIcons = screen.getAllByTestId('eds-icon-path')
+  expect(actionIcons).toBeDefined()
 })
