de3bb61f3ff63027ec5a79f56a8ba157be5fe584
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-04-20 15:58:14 +0200

    chore: define tabs routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, NonIndexRouteObject } from 'react-router-dom'
 import App from './App'
 
-export const router = createBrowserRouter([
+interface Tab extends Required<Pick<NonIndexRouteObject, 'path' | 'element'>> {
+  title: string
+}
+
+const NotImplemented = () => <p>Not implemented</p>
+
+const tabs: Tab[] = [
+  { title: 'Add model', path: 'add-model', element: <NotImplemented /> },
+  { title: 'Browse', path: 'browse', element: <NotImplemented /> },
+  { title: 'API', path: 'api', element: <NotImplemented /> },
+  { title: 'About', path: 'about', element: <NotImplemented /> },
+]
+
+const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: tabs,
   },
   {
     path: '*',
     element: <p>Invalid url</p>,
   },
 ])
+
+export { router, tabs }
