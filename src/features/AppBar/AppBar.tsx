eba7f7217c6778e9123329dcbebfc844410b52b6
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-04-20 16:01:52 +0200

    feat: add navigation to tabs routes, closes #31

--- src/features/AppBar/AppBar.tsx
+++ src/features/AppBar/AppBar.tsx
@@ -4,17 +4,29 @@ import {
   account_circle as accountCircle,
   notifications,
 } from '@equinor/eds-icons'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import MenuIcon from '../../components/MenuIcon/MenuIcon'
+import { tabs } from '../../router'
 import * as Styled from './AppBar.styled'
 
 const NavigationTabs = () => {
+  const navigate = useNavigate()
+  const [activeTab, setActiveTab] = useState<number>()
+
+  function clickTab(tab: number) {
+    navigate(tabs[tab].path)
+    setActiveTab(tab)
+  }
+
   return (
-    <Styled.Tabs>
+    <Styled.Tabs activeTab={activeTab} onChange={clickTab}>
       <Tabs.List>
-        <Tabs.Tab>Add model</Tabs.Tab>
-        <Tabs.Tab>Browse</Tabs.Tab>
-        <Tabs.Tab>API</Tabs.Tab>
-        <Tabs.Tab>About</Tabs.Tab>
+        {tabs.map((tab) => (
+          // TODO: Ensure that default accessibility concerns are met
+          // This doesn't support browser's default behaviour to "open in new tab"
+          <Tabs.Tab key={tab.title}>{tab.title}</Tabs.Tab>
+        ))}
       </Tabs.List>
     </Styled.Tabs>
   )
