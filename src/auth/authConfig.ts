4e9b8893fd5696abb6e2cb2be867bf56aef2ca81
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-05-04 12:19:22 +0200

    chore: configure api

--- src/auth/authConfig.ts
+++ src/auth/authConfig.ts
@@ -8,11 +8,25 @@ export const msalConfig: Configuration = {
     redirectUri: process.env.REACT_APP_AAD_REDIRECT_URI,
     postLogoutRedirectUri: process.env.REACT_APP_AAD_REDIRECT_URI,
   },
+  cache: {
+    cacheLocation: 'sessionStorage',
+  },
+}
+
+const protectedResources = {
+  endpoint: process.env.REACT_APP_BACKEND_URL,
+  scopes: {
+    read: [process.env.REACT_APP_BACKEND_API_SCOPE as string],
+  },
 }
 
 // Scopes for id token to be used at MS Identity Platform endpoints
 export const loginRequest: PopupRequest = {
-  scopes: ['User.Read'],
+  scopes: [...protectedResources.scopes.read],
+}
+
+export const apiConfig = {
+  baseUrl: `${process.env.REACT_APP_BACKEND_URL}/api`,
 }
 
 // Endpoints for MS Graph API services
