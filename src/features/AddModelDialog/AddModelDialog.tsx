849add71cdc62029397902e32fb4179085104121
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-02 10:50:49 +0200

    feat: add table to manage model input files

--- src/features/AddModelDialog/AddModelDialog.tsx
+++ src/features/AddModelDialog/AddModelDialog.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@equinor/eds-core-react'
+import { ModelInputFilesTable } from '../ModelInputFiles/ModelInputFiles'
 import * as Styled from './AddModelDialog.styled'
 
 interface AddModelDialogProps {
@@ -12,12 +13,20 @@ export const AddModelDialog = ({
   confirm,
   cancel,
 }: AddModelDialogProps) => {
+  // Temporary ignore because not implemented yet
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  function deleteInputFile() {}
+
   return (
     <Styled.Dialog open={isOpen}>
       <Styled.Dialog.Header>
         <Styled.Dialog.Title>Add new model</Styled.Dialog.Title>
       </Styled.Dialog.Header>
       <Styled.Dialog.CustomContent>
+        <ModelInputFilesTable
+          inputFiles={[{ name: 'CoarseSand_LargerFlow_1.nc', size: 1.43 }]}
+          onDeleteFile={deleteInputFile}
+        />
         <Styled.DialogActions>
           <Button onClick={confirm}>Confirm and start uploading</Button>
           <Button variant="outlined" onClick={cancel}>
