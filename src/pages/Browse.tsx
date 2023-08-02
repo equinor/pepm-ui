0d487cc8187ae97e71a0dbffed6274138edecd16
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-02 11:24:59 +0200

    chore: update routes to latest design

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,12 +1,28 @@
+import { Button } from '@equinor/eds-core-react'
+import { useState } from 'react'
 import { Table } from '../components/Table'
+import { AddModelDialog } from '../features/AddModelDialog/AddModelDialog'
 
 export const Browse = () => {
+  const [isAddModelDialog, setAddModelDialog] = useState<boolean>(false)
+
+  function closeDialog() {
+    setAddModelDialog(false)
+  }
+
+  // Temporary ignore because not implemented yet
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  function uploadModel() {}
+
   return (
     <>
-      {/* models?.map((model) => (
-        <p key={model.name}>{model.name}</p>
-      ))*/}
+      <Button onClick={() => setAddModelDialog(true)}>Add new model</Button>
       <Table />
+      <AddModelDialog
+        isOpen={isAddModelDialog}
+        confirm={uploadModel}
+        cancel={closeDialog}
+      />
     </>
   )
 }
