237eddf123163556c1112c1ddb29227c0457f6c0
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-02 10:07:14 +0200

    feat: add new model dialog

---
+++ src/features/AddModelDialog/AddModelDialog.styled.ts
@@ -0,0 +1,13 @@
+import { Dialog } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+import { spacings } from '../../tokens/spacings'
+
+const StyledDialog = styled(Dialog)`
+  width: 30em;
+`
+
+const StyledDialogActions = styled(Dialog.Actions)`
+  display: flex;
+  gap: ${spacings.SMALL};
+`
+export { StyledDialog as Dialog, StyledDialogActions as DialogActions }
