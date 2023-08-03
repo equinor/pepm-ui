b6416a91c6c8d29a4aabb975840e616e9a9d13ff
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-03 09:33:21 +0200

    feat: add description and metadata form

--- src/features/AddModelDialog/AddModelDialog.styled.ts
+++ src/features/AddModelDialog/AddModelDialog.styled.ts
@@ -6,8 +6,32 @@ const StyledDialog = styled(Dialog)`
   width: max-content;
 `
 
+const StyledDialogCustomContent = styled(Dialog.CustomContent)`
+  display: grid;
+  row-gap: ${spacings.X_LARGE};
+`
+
 const StyledDialogActions = styled(Dialog.Actions)`
   display: flex;
   gap: ${spacings.SMALL};
 `
-export { StyledDialog as Dialog, StyledDialogActions as DialogActions }
+
+const StyledMetadata = styled.div`
+  &.model-metadata {
+    display: grid;
+    grid-template-columns: auto auto;
+    column-gap: ${spacings.MEDIUM}};
+    row-gap: ${spacings.MEDIUM};
+
+    > .model-description {
+      grid-column: 1 / span 2;
+    }
+  }
+`
+
+export {
+  StyledDialog as Dialog,
+  StyledDialogActions as DialogActions,
+  StyledDialogCustomContent as DialogCustomContent,
+  StyledMetadata as Metadata,
+}
--- src/features/AddModelDialog/AddModelDialog.tsx
+++ src/features/AddModelDialog/AddModelDialog.tsx
@@ -1,4 +1,9 @@
-import { Button } from '@equinor/eds-core-react'
+import {
+  Autocomplete,
+  Button,
+  TextField,
+  Typography,
+} from '@equinor/eds-core-react'
 import { ModelInputFilesTable } from '../ModelInputFiles/ModelInputFiles'
 import * as Styled from './AddModelDialog.styled'
 
@@ -6,6 +11,25 @@ interface AddModelDialogProps {
   isOpen: boolean
   confirm: () => void
   cancel: () => void
+}
+
+const DescriptionAndMetadata = () => {
+  const fields = [{ name: 'Tor' }]
+  return (
+    <Styled.Metadata className="model-metadata">
+      <Typography variant="h4">Description and metadata</Typography>
+      <TextField
+        id="model-description"
+        className="model-description"
+        label="Model description (optional)"
+        multiline
+      />
+      <Autocomplete label="Field" options={fields}></Autocomplete>
+      <Autocomplete label="Formation" options={fields}></Autocomplete>
+      <Autocomplete label="Analogue (optional)" options={fields}></Autocomplete>
+      <Autocomplete label="Zone (optional)" options={fields}></Autocomplete>
+    </Styled.Metadata>
+  )
 }
 
 export const AddModelDialog = ({
@@ -22,18 +46,19 @@ export const AddModelDialog = ({
       <Styled.Dialog.Header>
         <Styled.Dialog.Title>Add new model</Styled.Dialog.Title>
       </Styled.Dialog.Header>
-      <Styled.Dialog.CustomContent>
+      <Styled.DialogCustomContent>
         <ModelInputFilesTable
           inputFiles={[{ name: 'CoarseSand_LargerFlow_1.nc', size: 1.43 }]}
           onDeleteFile={deleteInputFile}
         />
+        <DescriptionAndMetadata />
         <Styled.DialogActions>
           <Button onClick={confirm}>Confirm and start uploading</Button>
           <Button variant="outlined" onClick={cancel}>
             Cancel
           </Button>
         </Styled.DialogActions>
-      </Styled.Dialog.CustomContent>
+      </Styled.DialogCustomContent>
     </Styled.Dialog>
   )
 }
