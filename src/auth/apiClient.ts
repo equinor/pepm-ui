b99063ee5c035a4cfd47562fcf707135a18a8832
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-25 13:13:11 +0200

    refactor: only use msalInstance within MsalProvider

--- src/auth/apiClient.ts
+++ src/auth/apiClient.ts
@@ -4,6 +4,7 @@ import { apiConfig } from './authConfig'
 
 export const apiClient = createClient<paths>({
   baseUrl: apiConfig.baseUrl,
+  // headers: new Headers({ Authorization: `Bearer ${token}` }),
 })
 
 export type ApiClient = typeof apiClient
