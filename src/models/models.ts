43f20e63dfc33ad2b22ef9afbe0a73a26a4564b7
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-25 13:19:17 +0200

    refactor: remove unused file

--- src/models/models.ts
+++
@@ -1,6 +0,0 @@
-import { components, paths } from './schema'
-
-export type AnalogueModel = components['schemas']['AnalogueModelList']
-
-export type SuccessResponse =
-  paths['/api/analoguemodels']['get']['responses']['200']['content']['application/json']
