ba5d989bab6ee1cb03b8fd0a2e5fbaae8999da95
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-29 16:23:43 +0200

    refactor: use file uploader and common file column component

--- src/features/AddModelDialog/InputFilesTable/InputFilesTable.tsx
+++ src/features/AddModelDialog/InputFilesTable/InputFilesTable.tsx
@@ -1,14 +1,10 @@
-import { Button, Icon, Table } from '@equinor/eds-core-react'
-import {
-  subdirectory_arrow_right as arrowIcon,
-  delete_to_trash as deleteIcon,
-} from '@equinor/eds-icons'
+import { Button, Table } from '@equinor/eds-core-react'
+import { delete_to_trash as deleteIcon } from '@equinor/eds-icons'
 import IconButton from '../../../components/IconButton/IconButton'
-import { theme } from '../../../tokens/theme'
-import * as Styled from './InputFilesTable.styled'
+import { FileUploader } from '../../FileUploader/FileUploader'
 
-// Temporary type
-type File = { name: string; size: number; onDelete: () => void }
+// TODO: Temporary type
+export type File = { name: string; size: number; onDelete: () => void }
 
 type FileDisplay = { isVisible: boolean; toggle: () => void }
 
@@ -16,46 +12,33 @@ const DeleteButton = ({ onDelete }: { onDelete: () => void }) => (
   <IconButton icon={deleteIcon} title="delete" onClick={onDelete} />
 )
 
-const ModelFileColumn = ({ file }: { file?: File }) => (
-  <Table.Row className="nc-file">
-    <Table.Cell>
-      {file ? file.name : <a href="/">Select model NC file</a>}
-    </Table.Cell>
-    <Table.Cell />
-    <Table.Cell>{file ? `${file.size} GB` : '-'}</Table.Cell>
-    <Table.Cell>{file && <DeleteButton onDelete={file.onDelete} />}</Table.Cell>
-  </Table.Row>
-)
-
-const ParameterFileColumn = ({
+const FileColumn = ({
   file,
   fileDisplay,
 }: {
   file?: File
   fileDisplay?: FileDisplay
-}) => (
-  <Table.Row className="ini-file">
-    <Styled.TableCell>
-      {file ? (
-        file.name
-      ) : (
-        <>
-          <Icon fill={theme.light.text.staticIconsTertiary} data={arrowIcon} />
-          <a href="/">Select parameter INI file</a> (optional)
-        </>
-      )}
-    </Styled.TableCell>
-    <Table.Cell>
-      {file && (
-        <Button variant="outlined" onClick={fileDisplay?.toggle}>
-          {fileDisplay?.isVisible ? 'Hide' : 'Show'}
-        </Button>
-      )}
-    </Table.Cell>
-    <Table.Cell>{file ? `${file.size} GB` : '-'}</Table.Cell>
-    <Table.Cell>{file && <DeleteButton onDelete={file.onDelete} />}</Table.Cell>
-  </Table.Row>
-)
+}) => {
+  const isINI = fileDisplay !== undefined
+  return (
+    <Table.Row className={`${isINI ? 'ini' : 'nc'}-file`}>
+      <Table.Cell>
+        <FileUploader INI={isINI} file={file} />
+      </Table.Cell>
+      <Table.Cell>
+        {file && isINI && (
+          <Button variant="outlined" onClick={fileDisplay?.toggle}>
+            {fileDisplay?.isVisible ? 'Hide' : 'Show'}
+          </Button>
+        )}
+      </Table.Cell>
+      <Table.Cell>{file ? `${file.size} GB` : '-'}</Table.Cell>
+      <Table.Cell>
+        {file && <DeleteButton onDelete={file.onDelete} />}
+      </Table.Cell>
+    </Table.Row>
+  )
+}
 
 export const InputFilesTable = ({
   files,
@@ -75,8 +58,8 @@ export const InputFilesTable = ({
         </Table.Row>
       </Table.Head>
       <Table.Body>
-        <ModelFileColumn file={files.NC} />
-        <ParameterFileColumn file={files.INI} fileDisplay={fileDisplay} />
+        <FileColumn file={files.NC} />
+        <FileColumn file={files.INI} fileDisplay={fileDisplay} />
       </Table.Body>
     </Table>
   )
