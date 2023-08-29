2973f63b6ecc2842ee4c8f51068fe91fff9b56b9
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-08-29 16:17:25 +0200

    feat: add FileUploader

---
+++ src/features/FileUploader/FileUploader.styled.ts
@@ -0,0 +1,21 @@
+import { Typography } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+import { spacings } from '../../tokens/spacings'
+
+const FileUpload = styled.label`
+  display: flex;
+  align-items: center;
+  gap: ${spacings.X_SMALL};
+
+  > input[type='file'] {
+    display: none;
+  }
+`
+
+const SelectFile = styled(Typography).attrs({ variant: 'body_short_link' })`
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 14px;
+`
+
+export { FileUpload, SelectFile }
