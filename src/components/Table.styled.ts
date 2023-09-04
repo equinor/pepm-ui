b2524080ff66adc92853ad976e4ecbf14e26e51d
mheggelund <maohe@equinor.com>
2023-09-04 14:49:02 +0200

    fix: Added scrollbehaviour on table

---
+++ src/components/Table.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const StyledDiv = styled.div`
+  overflow-x: auto;
+  padding-bottom: 15px;
+`
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,7 @@
 import { Chip } from '@equinor/eds-core-react'
-import { EdsDataGrid } from '@equinor/eds-data-grid-react'
 import { useGetAnalogueModels } from '../hooks/useGetAnalogueModels'
+import * as Styled from './Table.styled'
+import { EdsDataGrid } from '@equinor/eds-data-grid-react'
 
 export const Table = () => {
   const { data: models } = useGetAnalogueModels()
@@ -8,54 +9,56 @@ export const Table = () => {
   if (!models) return <p>Loading...</p>
 
   return (
-    <EdsDataGrid
-      enableSorting
-      enablePagination
-      emptyMessage="Empty :("
-      rows={models}
-      columns={[
-        {
-          accessorKey: 'analogueModelId',
-          header: 'Model ID',
-          id: 'analogueModelId',
-        },
-        { accessorKey: 'name', header: 'Name', id: 'name' },
-        {
-          accessorKey: 'description',
-          header: 'Description',
-          id: 'description',
-        },
-        {
-          accessorKey: 'isApproved',
-          header: 'Result',
-          cell: () => <Chip>{'Approved'}</Chip>,
-        },
-        {
-          accessorKey: 'modified',
-          header: 'Last Modified',
-          cell: () => <div>{'<Last Modified>'}</div>,
-        },
-        {
-          accessorKey: 'analogue',
-          header: 'Analogue',
-          cell: () => <div>{'<Analogue>'}</div>,
-        },
-        {
-          accessorKey: 'formation',
-          header: 'Formation',
-          cell: () => <div>{'<Formation>'}</div>,
-        },
-        {
-          accessorKey: 'field',
-          header: 'Field',
-          cell: () => <div>{'<Field>'}</div>,
-        },
-        {
-          accessorKey: 'isProcessed',
-          header: 'Status',
-          id: 'isProcessed',
-        },
-      ]}
-    />
+    <Styled.StyledDiv>
+      <EdsDataGrid
+        enableSorting
+        enablePagination
+        emptyMessage="Empty :("
+        rows={models}
+        columns={[
+          {
+            accessorKey: 'analogueModelId',
+            header: 'Model ID',
+            id: 'analogueModelId',
+          },
+          { accessorKey: 'name', header: 'Name', id: 'name' },
+          {
+            accessorKey: 'description',
+            header: 'Description',
+            id: 'description',
+          },
+          {
+            accessorKey: 'isApproved',
+            header: 'Result',
+            cell: () => <Chip>{'Approved'}</Chip>,
+          },
+          {
+            accessorKey: 'modified',
+            header: 'Last Modified',
+            cell: () => <div>{'<Last Modified>'}</div>,
+          },
+          {
+            accessorKey: 'analogue',
+            header: 'Analogue',
+            cell: () => <div>{'<Analogue>'}</div>,
+          },
+          {
+            accessorKey: 'formation',
+            header: 'Formation',
+            cell: () => <div>{'<Formation>'}</div>,
+          },
+          {
+            accessorKey: 'field',
+            header: 'Field',
+            cell: () => <div>{'<Field>'}</div>,
+          },
+          {
+            accessorKey: 'isProcessed',
+            header: 'Status',
+            id: 'isProcessed',
+          },
+        ]}
+      />
+    </Styled.StyledDiv>
   )
 }
