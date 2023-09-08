632d4cc3500dc1d1cc7dcbaeac3f0ba4d18b145e
mheggelund <maohe@equinor.com>
2023-09-08 08:13:52 +0200

    fix: uppdated responsive bahaviour of data table

--- src/components/Table.styled.ts
+++ src/components/Table.styled.ts
@@ -1,6 +1,18 @@
 import styled from 'styled-components'
+import { spacings } from '../tokens/spacings'
 
 export const StyledDiv = styled.div`
   overflow-x: auto;
-  padding-bottom: 15px;
+  padding-bottom: ${spacings.MEDIUM};
+
+  > .table-wrapper {
+    > table {
+      width: auto !important;
+      margin: 0px !important;
+    }
+
+    > div {
+      max-width: none !important;
+    }
+  }
 `
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import { Chip } from '@equinor/eds-core-react'
+import { EdsDataGrid } from '@equinor/eds-data-grid-react'
 import { useGetAnalogueModels } from '../hooks/useGetAnalogueModels'
 import * as Styled from './Table.styled'
-import { EdsDataGrid } from '@equinor/eds-data-grid-react'
 
 export const Table = () => {
   const { data: models } = useGetAnalogueModels()
