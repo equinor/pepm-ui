b1c0f950a89acc474dec7832054abd0bed9d23d9
mheggelund <maohe@equinor.com>
2023-09-21 09:40:53 +0200

    chore: fixed and improved responsive styling

--- src/features/ModelView/ModelMetadataView/ModelMetadataView.tsx
+++ src/features/ModelView/ModelMetadataView/ModelMetadataView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAnalogueModels } from '../../../hooks/useAnalogueModels'
@@ -35,6 +36,21 @@ export const ModelMetadataView = () => {
         <p>
           {model.description}
           <br />
+          <p>
+            ** Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            bibendum ex at venenatis gravida. Sed id tempor dui. Nunc a posuere
+            ligula. Pellentesque pulvinar varius neque nec molestie. Aliquam
+            erat volutpat. Nunc pulvinar varius scelerisque. Suspendisse
+            iaculis, elit id fringilla semper, justo felis luctus felis, et
+            malesuada augue sapien a sem. Donec varius, sapien quis varius
+            blandit, justo ex pellentesque nisl, eu placerat magna nisi et odio.
+            Donec laoreet est quam, id fringilla magna semper in. Duis non massa
+            euismod, ultrices tortor et, ultricies ante. Vivamus quis dignissim
+            sem. Quisque purus dui, euismod eu lacus sed, mollis sagittis arcu.
+            Curabitur vitae mauris ornare, elementum massa suscipit, congue leo.
+            Sed fermentum imperdiet dapibus. Aliquam non ligula in felis laoreet
+            suscipit. **
+          </p>
         </p>
       )}
       <div>
