59f9bad5832af9e1c522aa8afca031bff4c46746
mheggelund <maohe@equinor.com>
2023-09-25 10:35:41 +0200

    chore/added infor page component

---
+++ src/components/InfoPageComponent/InfoPageComponent.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@equinor/eds-core-react'
+import * as Styled from './InfoPageComponent.style'
+
+export const InfoPageComponent = ({
+  title,
+  children,
+}: {
+  title: string
+  children?: JSX.Element[]
+}) => {
+  return (
+    <Styled.Page className="about-container">
+      <Styled.InnerWrapper>
+        <Typography variant="h1">{title}</Typography>
+        {children}
+      </Styled.InnerWrapper>
+    </Styled.Page>
+  )
+}
