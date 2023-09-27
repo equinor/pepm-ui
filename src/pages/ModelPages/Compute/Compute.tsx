4a7ec22916458b816b7530644bf4acef7e1ecd59
mheggelund <maohe@equinor.com>
2023-09-27 13:48:37 +0200

    refactor: Add ComputeVariogram and ComputeObject pages

--- src/pages/ModelPages/Compute/Compute.tsx
+++
@@ -1,7 +0,0 @@
-export const Compute = () => {
-  return (
-    <>
-      <p>Options to compute models will soon be possible here!</p>
-    </>
-  )
-}
---
+++ src/pages/ModelPages/Compute/ComputeVariogram.tsx
@@ -0,0 +1,7 @@
+export const ComputeVariogram = () => {
+  return (
+    <>
+      <p>Options to compute variogram will soon be possible here!</p>
+    </>
+  )
+}
