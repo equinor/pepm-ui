9968a306dea91a5b55d8ea7581588c0212f2b5ae
mheggelund <maohe@equinor.com>
2023-10-05 15:38:19 +0200

    chore: add no results page

--- src/pages/ModelPages/Results/Results.tsx
+++ src/pages/ModelPages/Results/Results.tsx
@@ -1,7 +1,20 @@
+import { Typography } from '@equinor/eds-core-react'
+import { InfoPageComponent } from '../../../components/InfoPageComponent/InfoPageComponent'
+import NoResultPicture from './NoResults.jpeg'
+import * as Styled from './Results.styled'
+
 export const Results = () => {
   return (
-    <>
-      <p>Results for models will soon be available here!</p>
-    </>
+    <InfoPageComponent scaleHight="true">
+      <img src={NoResultPicture} alt="altText" />
+      <Styled.TextDiv>
+        <Typography variant="h4">
+          Oh no! It seems we don’t have any results for this model yet
+        </Typography>
+        <Typography variant="body_long">
+          To get started, try adding cases to the variogram or object page
+        </Typography>
+      </Styled.TextDiv>
+    </InfoPageComponent>
   )
 }
