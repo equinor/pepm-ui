1a2418ffaa20973e3ef656d5b5e90750b7d11153
mheggelund <maohe@equinor.com>
2023-10-06 08:53:49 +0200

    refactor: refactor Result-page and add NoResult component

--- src/pages/ModelPages/Results/Results.tsx
+++ src/pages/ModelPages/Results/Results.tsx
@@ -1,20 +1,7 @@
-import { Typography } from '@equinor/eds-core-react'
-import { InfoPageComponent } from '../../../components/InfoPageComponent/InfoPageComponent'
-import NoResultPicture from './NoResults.jpeg'
-import * as Styled from './Results.styled'
+import { NoResults } from '../../../features/Results/NoResults/NoResults'
 
 export const Results = () => {
-  return (
-    <InfoPageComponent scaleHight="true">
-      <img src={NoResultPicture} alt="altText" />
-      <Styled.TextDiv>
-        <Typography variant="h4">
-          Oh no! It seems we don’t have any results for this model yet
-        </Typography>
-        <Typography variant="body_long">
-          To get started, try adding cases to the variogram or object page
-        </Typography>
-      </Styled.TextDiv>
-    </InfoPageComponent>
-  )
+  const results = undefined
+
+  return <>{results ? <p>Results Here</p> : <NoResults />}</>
 }
