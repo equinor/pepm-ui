145e8e082ab7d351e6494844f1d14aa129e3b533
mheggelund <maohe@equinor.com>
2023-10-12 10:18:02 +0200

    chore: Add list and view of case results.

--- src/pages/ModelPages/Results/Results.tsx
+++ src/pages/ModelPages/Results/Results.tsx
@@ -1,7 +1,46 @@
+import { useNavigate } from 'react-router-dom'
 import { NoResults } from '../../../features/Results/NoResults/NoResults'
+import * as Styled from './Results.styled'
 
+export type ResultType = {
+  id: string
+  case: string
+  finished: boolean
+}
 export const Results = () => {
-  const results = undefined
+  const loaded = true
+  const results: ResultType[] = [
+    { id: '1', case: 'Variogramcase 1', finished: true },
+    { id: '2', case: 'Variogramcase 2', finished: false },
+  ]
 
-  return <>{results ? <p>Results Here</p> : <NoResults />}</>
+  return (
+    <>
+      {loaded && results ? <CaseResultList results={results} /> : <NoResults />}
+    </>
+  )
+}
+
+const CaseResultList = ({ results }: { results: ResultType[] }) => {
+  return (
+    <Styled.ResultCaseList>
+      {results.map((res: ResultType) => (
+        <ResultCaseCard key={res.id} result={res} />
+      ))}
+    </Styled.ResultCaseList>
+  )
+}
+
+const ResultCaseCard = ({ result }: { result: ResultType }) => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(`${result.id}`)
+  }
+
+  return (
+    <Styled.ResultCaseCard onClick={handleClick}>
+      <h2>{result.case}</h2>
+      {result.finished ? <h4>Finished</h4> : <h4>Not computed yet!</h4>}
+    </Styled.ResultCaseCard>
+  )
 }
