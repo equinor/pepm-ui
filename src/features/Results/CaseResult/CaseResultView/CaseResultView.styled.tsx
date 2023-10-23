c9a87a9c91862afe2f9d850a315ce0f988bfb235
mheggelund <maohe@equinor.com>
2023-10-23 14:00:43 +0200

    chore: Add simple case result page with hardcoded data

--- src/features/Results/CaseResult/CaseResultView/CaseResultView.styled.tsx
+++ src/features/Results/CaseResult/CaseResultView/CaseResultView.styled.tsx
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
 import { spacings } from '../../../../tokens/spacings';
+import { theme } from '../../../../tokens/theme';
+
 export const CaseResultView = styled.div`
   display: flex;
   flex-direction: column;
 
-  column-gap: ${spacings.LARGE};
+  row-gap: ${spacings.XXX_LARGE};
   padding-left: ${spacings.LARGE};
+`;
+
+export const CaseResultCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${spacings.LARGE};
+
+  width: 60vw;
+  padding: ${spacings.X_LARGE};
+
+  border-radius: ${spacings.CARD_ROUNDED};
+  box-shadow: ${theme.light.ui.elevation.raised};
+
+  > div {
+    width: 50%;
+  }
+
+  > table {
+    width: 50%;
+  }
 `;
--- src/features/Results/CaseResult/CaseResultView/CaseResultView.tsx
+++ src/features/Results/CaseResult/CaseResultView/CaseResultView.tsx
@@ -1,6 +1,9 @@
+import { Table } from '@equinor/eds-core-react';
+import { ImageView } from '../../../../components/ImageView/ImageView';
 import { ResultType } from '../../../../pages/ModelPages/Results/Results';
 import * as Styled from './CaseResultView.styled';
-
+import Img from './vargrest_output-0-_variogram_slices_.png';
+import Img1 from './vargrest_output-1-_variogram_slices_.png';
 export const CaseResultView = () => {
   const caseResult: ResultType = {
     id: '1',
@@ -13,12 +16,64 @@ export const CaseResultView = () => {
   // ]
 
   return (
-    <>
-      <Styled.CaseResultView>
-        <h2>Case Result</h2>
-        <h3>{caseResult.case}</h3>
-        {caseResult.finished ? <h4>Finished</h4> : <h4>Not computed yet!</h4>}
-      </Styled.CaseResultView>
-    </>
+    <Styled.CaseResultView>
+      <h2>Case Results</h2>
+      <h3>{caseResult.case}</h3>
+      <ResultMetadata caseNumner={0} img={Img}></ResultMetadata>
+      <ResultMetadata caseNumner={1} img={Img1}></ResultMetadata>
+    </Styled.CaseResultView>
+  );
+};
+
+const ResultMetadata = ({
+  caseNumner,
+  img,
+}: {
+  caseNumner: number;
+  img: string;
+}) => {
+  const data = [
+    {
+      identifier: 0,
+      family: 'exponential',
+      indicator: null,
+      attribute: 'Porosity',
+      quality: 0.6427819811789964,
+      sigma: 0.06967589201242001,
+    },
+    {
+      identifier: 1,
+      family: 'gaussian',
+      indicator: null,
+      attribute: 'Porosity',
+      quality: 0.5432924009373808,
+      sigma: 0.0670758033212357,
+    },
+  ];
+
+  return (
+    <Styled.CaseResultCard>
+      <Table>
+        <Table.Body>
+          <Table.Row key={data[caseNumner].identifier} className="table-row">
+            <Table.Cell className="table-first-col">Quality factor</Table.Cell>
+            <Table.Cell>{data[caseNumner].quality}</Table.Cell>
+          </Table.Row>
+          <Table.Row key={data[caseNumner].identifier} className="table-row">
+            <Table.Cell className="table-first-col">Family</Table.Cell>
+            <Table.Cell>{data[caseNumner].family}</Table.Cell>
+          </Table.Row>
+          <Table.Row key={data[caseNumner].identifier} className="table-row">
+            <Table.Cell className="table-first-col">Attribute</Table.Cell>
+            <Table.Cell>{data[caseNumner].attribute}</Table.Cell>
+          </Table.Row>
+          <Table.Row key={data[caseNumner].identifier} className="table-row">
+            <Table.Cell className="table-first-col">Sigma</Table.Cell>
+            <Table.Cell>{data[caseNumner].sigma}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <ImageView text="run" img={img} altText="run"></ImageView>
+    </Styled.CaseResultCard>
   );
 };
