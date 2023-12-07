b0f6e8b8b88c67b4d99330345cbc0f145f4fccf2
mheggelund <maohe@equinor.com>
2023-12-07 08:54:16 +0100

    refactor: Seperated MetadataSelect into own component.

---
+++ src/features/AddModel/ModelMetadata/MetadataSelect/MetadataSelect.tsx
@@ -0,0 +1,48 @@
+import { Autocomplete, AutocompleteChanges } from '@equinor/eds-core-react';
+import { MetadataDto } from '../../../../api/generated';
+
+export const MetadataSelect = ({
+  type,
+  errors,
+  data,
+  metadata,
+  isLoading,
+  handleAddMetadata,
+}: {
+  type: string;
+  errors: string | undefined;
+  data: MetadataDto[];
+  metadata: MetadataDto[];
+  isLoading: boolean;
+  handleAddMetadata: (
+    e: AutocompleteChanges<MetadataDto>,
+    type: string,
+  ) => void;
+}) => {
+  const setSelectedMetadataOptions = (type: string) => {
+    if (!isLoading && data) {
+      const dataProps = data.filter((z) => z.metadataType === type);
+
+      const selectedProps = metadata.filter((m) => m.metadataType === type);
+
+      return dataProps.filter(
+        (c) =>
+          selectedProps.findIndex((x: MetadataDto) => x.value === c.value) > -1,
+      );
+    }
+  };
+
+  return (
+    <Autocomplete
+      variant={errors ? 'error' : undefined}
+      label={type}
+      options={data.filter((d) => d.metadataType === type)}
+      optionLabel={(option) => option.value}
+      selectedOptions={setSelectedMetadataOptions(type)}
+      multiple
+      onOptionsChange={(e: AutocompleteChanges<MetadataDto>) =>
+        handleAddMetadata(e, type)
+      }
+    ></Autocomplete>
+  );
+};
