97bc0f5a054c3753eef3a13e8effa08fa084f889
mheggelund <maohe@equinor.com>
2023-12-12 10:32:23 +0100

    refactor: Refactor area coordinates styling and logic.

--- src/components/AreaCoordinates/AreaCoordinates.styled.tsx
+++ src/components/AreaCoordinates/AreaCoordinates.styled.tsx
@@ -1,4 +1,3 @@
-import { Typography } from '@equinor/eds-core-react';
 import styled from 'styled-components';
 import { spacings } from '../../tokens/spacings';
 
@@ -12,18 +11,21 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
 
-  row-gap: ${spacings.LARGE};
+  row-gap: ${spacings.X_LARGE};
 `;
 
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${spacings.MEDIUM};
+`;
 export const CoordinateGroup = styled.div`
   display: flex;
   flex-direction: column;
-
-  > .error {
-    color: red;
-  }
 `;
 
-export const ErrorMessage = styled(Typography)`
-  color: red;
+export const CoordinateInputs = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: ${spacings.MEDIUM};
 `;
--- src/components/AreaCoordinates/AreaCoordinates.tsx
+++ src/components/AreaCoordinates/AreaCoordinates.tsx
@@ -6,11 +6,13 @@ import {
   Autocomplete,
   AutocompleteChanges,
   Button,
+  Icon,
   Input,
   Label,
   Snackbar,
   Typography,
 } from '@equinor/eds-core-react';
+import { error_filled as ERROR_FILLED } from '@equinor/eds-icons';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import {
@@ -52,15 +54,42 @@ const defaultState: AreaCoordinateType = {
   ],
 };
 
+const defaultArea: ModelAreaTypeDto = {
+  description: '',
+  modelAreaTypeId: '',
+  name: '',
+};
+
 export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
   const [showSaveAlert, setSaveAlert] = useState(false);
-  const [activeArea, setActiveArea] = useState<ModelAreaTypeDto>();
+  const [activeArea, setActiveArea] = useState<ModelAreaTypeDto>(defaultArea);
+  const [newArea, setNewArea] = useState<ModelAreaTypeDto>();
   const [updated, setUpdated] = useState(1);
   const [errors, setErrors] = useState<ErrorType>({});
   const [submitting, setSubmitting] = useState(false);
-  const [areaCoodrinate, setAreaCoodrinate] = useState<
-    AreaCoordinateType | undefined
-  >(defaultState);
+  const [areaCoordinate, setAreaCoordinate] = useState<AreaCoordinateType>({
+    modelAreaId: '',
+    coordinates: [
+      {
+        x: 0,
+        y: 0,
+        m: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+        m: 1,
+      },
+    ],
+  });
+
+  function clearStatus() {
+    setSaveAlert(false);
+  }
+
+  function isNumber(value: any) {
+    return isNaN(value);
+  }
 
   const model = useQuery({
     queryKey: ['analogue-models', modelId, updated],
@@ -105,99 +134,29 @@ export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
     },
   });
 
-  function clearStatus() {
-    setSaveAlert(false);
-  }
-
-  function isNumber(value: any) {
-    return isNaN(value);
-  }
-
-  const validateCoordinates = (area: AreaCoordinateType | undefined) => {
-    const errors: ErrorType = {};
-    if (area && area.coordinates[0].x === area.coordinates[1].x) {
-      errors.x0 = 'X coodridnates can´t be equal.';
-      errors.x1 = 'X coodridnates can´t be equal.';
-    }
-    if (area && area.coordinates[0].y === area.coordinates[1].y) {
-      errors.y0 = 'X coodridnates can´t be equal.';
-      errors.y1 = 'X coodridnates can´t be equal.';
-    }
-    if (area && isNumber(area.coordinates[0].x)) {
-      errors.x0 = 'Coodridnates can´t be string, just numbers is alowed.';
-    }
-    if (area && isNumber(area.coordinates[0].y)) {
-      errors.y0 = 'Coodridnates can´t be string, just numbers is alowed.';
-    }
-    if (area && isNumber(area.coordinates[1].x)) {
-      errors.x1 = 'Coodridnates can´t be string, just numbers is alowed.';
-    }
-    if (area && isNumber(area.coordinates[1].y)) {
-      errors.y1 = 'Coodridnates can´t be string, just numbers is alowed.';
-    }
-    if (area && area.coordinates[1].x === 0) {
-      errors.x1 = 'Bottom right conrner can not be 0.';
-    }
-    if (area && area.coordinates[1].y === 0) {
-      errors.y1 = 'Bottom right conrner can not be 0.';
-    }
-    return errors;
-  };
-
-  const postModelArea = async () => {
-    if (activeArea && areaCoodrinate) {
-      const postRequestBody: AddAnalogueModelAreaCommandForm = {
-        modelAreaTypeId: activeArea.modelAreaTypeId,
-        coordinates: areaCoodrinate.coordinates,
-      };
-
-      const coordinateRes = await postAreaCoordinates.mutateAsync({
-        id: modelId,
-        requestBody: postRequestBody,
-      });
-
-      if (coordinateRes.success) {
-        setUpdated(updated + 1);
-        setActiveArea(undefined);
-        setAreaCoodrinate(defaultState);
-        setSaveAlert(true);
-      }
-    }
-  };
-
-  const handleSubmit = () => {
-    setErrors(validateCoordinates(areaCoodrinate));
-    setSubmitting(true);
-  };
-
-  const handleSave = async () => {
-    if (!areaCoodrinate || !areaCoodrinate.coordinates) {
-      return;
-    }
-
-    if (activeArea) {
-      postModelArea();
-    } else {
-      const coordinateRes = await putAreaCoordinates.mutateAsync({
-        id: modelId,
-        modelAreaId: areaCoodrinate.modelAreaId,
-        requestBody: { coordinates: areaCoodrinate.coordinates },
-      });
-      if (coordinateRes.success) {
-        setUpdated(updated + 1);
-        setAreaCoodrinate(defaultState);
-        setSaveAlert(true);
-      }
-    }
-  };
-
   const handleSelectChange = async (
     changes: AutocompleteChanges<ModelAreaTypeDto>,
   ) => {
     setSubmitting(false);
     setErrors({});
+
     if (changes.selectedItems.length <= 0) {
-      setAreaCoodrinate(defaultState);
+      setActiveArea(defaultArea);
+      setAreaCoordinate({
+        modelAreaId: '',
+        coordinates: [
+          {
+            x: 0,
+            y: 0,
+            m: 0,
+          },
+          {
+            x: 0,
+            y: 0,
+            m: 1,
+          },
+        ],
+      });
       return;
     }
 
@@ -209,10 +168,27 @@ export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
     );
 
     if (selectedArea?.length === 0) {
+      if (activeArea) {
+        setAreaCoordinate({
+          modelAreaId: '',
+          coordinates: [
+            {
+              x: 0,
+              y: 0,
+              m: 0,
+            },
+            {
+              x: 0,
+              y: 0,
+              m: 1,
+            },
+          ],
+        });
+      }
       setActiveArea(changes.selectedItems[0]);
-    }
-
-    if (selectedArea && selectedArea?.length > 0) {
+      setNewArea(changes.selectedItems[0]);
+    } else if (selectedArea && selectedArea?.length > 0) {
+      setNewArea(undefined);
       const newState: AreaCoordinateType = {
         modelAreaId: selectedArea[0].modelAreaId,
         coordinates: [
@@ -228,22 +204,160 @@ export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
           },
         ],
       };
-      setAreaCoodrinate(newState);
+
+      setActiveArea(changes.selectedItems[0]);
+      setAreaCoordinate(newState);
     } else {
-      setAreaCoodrinate(defaultState);
+      setNewArea(undefined);
+      setActiveArea(changes.selectedItems[0]);
+      setAreaCoordinate(defaultState);
     }
   };
 
-  const setCoodrinates = (target: any, index: number, axis: string) => {
-    if (!areaCoodrinate) return;
+  const validateCoordinates = (area: AreaCoordinateType | undefined) => {
+    const errors: ErrorType = {};
+    if (!activeArea || activeArea.modelAreaTypeId === '') {
+      errors.area = 'Moedl area needs to be selected';
+    }
+
+    if (area && area.coordinates[0].x === area.coordinates[1].x) {
+      errors.x0 = 'X coodridnates can´t be equal.';
+    }
+    if (area && area.coordinates[0].y === area.coordinates[1].y) {
+      errors.y0 = 'Y coodridnates can´t be equal.';
+    }
+    if (area && isNumber(area.coordinates[0].x)) {
+      errors.x0 = 'Coodridnates can´t be string, just numbers is alowed.';
+    }
+    if (area && isNumber(area.coordinates[0].y)) {
+      errors.y0 = 'Coodridnates can´t be string, just numbers is alowed.';
+    }
+    if (area && isNumber(area.coordinates[1].x)) {
+      errors.x1 = 'Coodridnates can´t be string, just numbers is alowed.';
+    }
+    if (area && isNumber(area.coordinates[1].y)) {
+      errors.y1 = 'Coodridnates can´t be string, just numbers is alowed.';
+    }
+    if (area && (area.coordinates[1].x === 0 || area.coordinates[1].y === 0)) {
+      errors.x1 = 'Bottom right conrner can not be 0.';
+      errors.y1 = 'Bottom right conrner can not be 0.';
+    }
+
+    if (
+      area &&
+      (area.coordinates[0].x === null ||
+        area.coordinates[0].x === undefined ||
+        // @ts-expect-error Autocomplete
+        area.coordinates[0].x === '')
+    ) {
+      errors.x0 = 'All fields munst be filled in';
+    }
+
+    if (
+      area &&
+      (area.coordinates[0].y === null ||
+        area.coordinates[0].y === undefined ||
+        // @ts-expect-error Autocomplete
+        area.coordinates[0].y === '')
+    ) {
+      errors.y0 = 'All fields munst be filled in';
+    }
+
+    if (
+      area &&
+      (area.coordinates[1].x === null ||
+        area.coordinates[1].x === undefined ||
+        // @ts-expect-error Autocomplete
+        area.coordinates[1].x === '')
+    ) {
+      errors.x1 = 'All fields munst be filled in';
+    }
+
+    if (
+      area &&
+      (area.coordinates[1].y === null ||
+        area.coordinates[1].y === undefined ||
+        // @ts-expect-error Autocomplete
+        area.coordinates[1].y === '')
+    ) {
+      errors.y1 = 'All fields munst be filled in';
+    }
+
+    return errors;
+  };
+
+  const postModelArea = async () => {
+    if (activeArea && areaCoordinate) {
+      const postRequestBody: AddAnalogueModelAreaCommandForm = {
+        modelAreaTypeId: activeArea.modelAreaTypeId,
+        coordinates: areaCoordinate.coordinates,
+      };
+
+      const coordinateRes = await postAreaCoordinates.mutateAsync({
+        id: modelId,
+        requestBody: postRequestBody,
+      });
+
+      if (coordinateRes.success) {
+        setActiveArea(defaultArea);
+        setAreaCoordinate({
+          modelAreaId: '',
+          coordinates: [
+            {
+              x: 0,
+              y: 0,
+              m: 0,
+            },
+            {
+              x: 0,
+              y: 0,
+              m: 1,
+            },
+          ],
+        });
+        setUpdated(updated + 1);
+        setSaveAlert(true);
+      }
+    }
+  };
+
+  const handleSubmit = () => {
+    setErrors(validateCoordinates(areaCoordinate));
+    setSubmitting(true);
+  };
+
+  const handleSave = async () => {
+    if (!areaCoordinate || !areaCoordinate.coordinates) {
+      return;
+    }
+    if (newArea) {
+      postModelArea();
+    } else {
+      const coordinateRes = await putAreaCoordinates.mutateAsync({
+        id: modelId,
+        modelAreaId: areaCoordinate.modelAreaId,
+        requestBody: { coordinates: areaCoordinate.coordinates },
+      });
+      if (coordinateRes.success) {
+        setUpdated(updated + 1);
+        setActiveArea(defaultArea);
+        setAreaCoordinate(defaultState);
+        setSaveAlert(true);
+        setNewArea(undefined);
+      }
+    }
+  };
+
+  const setCoordinates = (target: any, index: number, axis: string) => {
+    if (!areaCoordinate) return;
 
     const uppdatedArea = {
-      ...areaCoodrinate?.coordinates[index],
+      ...areaCoordinate?.coordinates[index],
       [axis]: target.value,
     };
-    const newCoordinates = { ...areaCoodrinate };
+    const newCoordinates = { ...areaCoordinate };
     newCoordinates.coordinates[index] = uppdatedArea;
-    setAreaCoodrinate(newCoordinates);
+    setAreaCoordinate(newCoordinates);
   };
 
   useEffect(() => {
@@ -259,98 +373,122 @@ export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
   return (
     <>
       <Styled.SideSheet>
-        {model.data?.success && (
-          <Typography variant="h2">{model.data.data.name}</Typography>
-        )}
-        <Typography variant="h3">Set coordinates: </Typography>
+        <Typography variant="h2">Model areas</Typography>
         <Styled.Form>
+          <Styled.Info>
+            <Typography variant="h6">
+              {model.data?.success && model.data.data.name}
+            </Typography>
+            <Typography variant="body_long">
+              Select from predefined model areas and set the X/Y coordinates for
+              each area.
+            </Typography>
+          </Styled.Info>
           <Styled.CoordinateGroup className={'autocomplete-error'}>
-            <Typography variant="h6">Area to define</Typography>
-
             <Autocomplete
               className={errors?.area && 'autocomplete-error'}
               label={'Select area'}
               options={modelAreas.data.data}
               optionLabel={(option) => option.name}
               onOptionsChange={handleSelectChange}
+              variant={errors.area ? 'error' : undefined}
             ></Autocomplete>
-            <div className={'autocomplete-error'}>{errors?.area}</div>
           </Styled.CoordinateGroup>
 
           <Styled.CoordinateGroup>
             <Typography variant="h6">Top Left Corner</Typography>
-            <div className={errors?.x0 && 'input-error'}>
-              <Label label="X-coordinate" />
-              <Input
-                value={areaCoodrinate?.coordinates[0].x}
-                onChange={(input: { target: any }) =>
-                  setCoodrinates(input.target, 0, 'x')
-                }
-              />
-              {errors?.x0}
-            </div>
-            <div className={errors?.y0 && 'input-error'}>
-              <Label label="Y-coordinate" />
-              <Input
-                value={areaCoodrinate?.coordinates[0].y}
-                onChange={(input: { target: any }) =>
-                  setCoodrinates(input.target, 0, 'y')
-                }
-              />
-              {errors?.y0}
-            </div>
-
-            {/* <CoordinateInput
-              errors={errors}
-              areaCoodrinate={areaCoodrinate}
-              setCoodrinates={setCoodrinates}
-              position={0}
-              axis="x"
-            />
-            <CoordinateInput
-              errors={errors}
-              areaCoodrinate={areaCoodrinate}
-              setCoodrinates={setCoodrinates}
-              position={0}
-              axis="y"
-            /> */}
+            <Styled.CoordinateInputs>
+              <div>
+                <Label label="X start" />
+                <Input
+                  value={areaCoordinate?.coordinates[0].x}
+                  onChange={(input: { target: any }) =>
+                    setCoordinates(input.target, 0, 'x')
+                  }
+                  rightAdornments={
+                    errors?.x0 ? (
+                      <Icon data={ERROR_FILLED} color="red" size={24}></Icon>
+                    ) : (
+                      'meters'
+                    )
+                  }
+                  variant={errors?.x0 ? 'error' : undefined}
+                  disabled={activeArea?.modelAreaTypeId === ''}
+                />
+              </div>
+              <div>
+                <Label label="Y start" />
+                <Input
+                  value={areaCoordinate?.coordinates[0].y}
+                  onChange={(input: { target: any }) =>
+                    setCoordinates(input.target, 0, 'y')
+                  }
+                  rightAdornments={
+                    errors?.y0 ? (
+                      <Icon data={ERROR_FILLED} color="red" size={24}></Icon>
+                    ) : (
+                      'meters'
+                    )
+                  }
+                  variant={errors?.y0 ? 'error' : undefined}
+                  disabled={activeArea?.modelAreaTypeId === ''}
+                />
+              </div>
+            </Styled.CoordinateInputs>
           </Styled.CoordinateGroup>
           <Styled.CoordinateGroup>
             <Typography variant="h6">Bottom Right Corner </Typography>
-            <div className={errors?.x1 && 'input-error'}>
-              <Label label="X-coordinate" />
-              <Input
-                value={areaCoodrinate?.coordinates[1].x}
-                onChange={(input: { target: any }) =>
-                  setCoodrinates(input.target, 1, 'x')
-                }
-              />
-              {errors?.x1}
+            <Styled.CoordinateInputs>
+              <div>
+                <Label label="X end" />
+                <Input
+                  value={areaCoordinate?.coordinates[1].x}
+                  onChange={(input: { target: any }) =>
+                    setCoordinates(input.target, 1, 'x')
+                  }
+                  rightAdornments={
+                    errors?.x1 ? (
+                      <Icon data={ERROR_FILLED} color="red" size={24}></Icon>
+                    ) : (
+                      'meters'
+                    )
+                  }
+                  variant={errors?.x1 ? 'error' : undefined}
+                  disabled={activeArea?.modelAreaTypeId === ''}
+                />
+              </div>
+              <div>
+                <Label label="Y end" />
+                <Input
+                  value={areaCoordinate?.coordinates[1].y}
+                  onChange={(input: { target: any }) =>
+                    setCoordinates(input.target, 1, 'y')
+                  }
+                  rightAdornments={
+                    errors?.y1 ? (
+                      <Icon data={ERROR_FILLED} color="red" size={24}></Icon>
+                    ) : (
+                      'meters'
+                    )
+                  }
+                  variant={errors?.y1 ? 'error' : undefined}
+                  disabled={activeArea?.modelAreaTypeId === ''}
+                />
+              </div>
+            </Styled.CoordinateInputs>
+
+            <div>
+              {errors && (
+                <div>
+                  {' '}
+                  <p>{errors.area}</p>
+                  <p>{errors.x0}</p>
+                  <p>{errors.y0}</p>
+                  <p>{errors.x1}</p>
+                  <p>{errors.y1}</p>
+                </div>
+              )}
             </div>
-            <div className={errors?.y1 && 'input-error'}>
-              <Label label="Y-coordinate" />
-              <Input
-                value={areaCoodrinate?.coordinates[1].y}
-                onChange={(input: { target: any }) =>
-                  setCoodrinates(input.target, 1, 'y')
-                }
-              />
-              {errors?.y1}
-            </div>
-            {/* <CoordinateInput
-              errors={errors}
-              areaCoodrinate={areaCoodrinate}
-              setCoodrinates={setCoodrinates}
-              position={1}
-              axis="x"
-            />
-            <CoordinateInput
-              errors={errors}
-              areaCoodrinate={areaCoodrinate}
-              setCoodrinates={setCoodrinates}
-              position={1}
-              axis="y"
-            /> */}
           </Styled.CoordinateGroup>
 
           <Button onClick={handleSubmit}>Save</Button>
@@ -361,39 +499,8 @@ export const AreaCoordinates = ({ modelId }: { modelId: string }) => {
         autoHideDuration={3000}
         onClose={clearStatus}
       >
-        {'Area coordinates saved'}
+        {'Saved model area'}
       </Snackbar>
     </>
   );
 };
-
-// const CoordinateInput = ({
-//   errors,
-//   areaCoodrinate,
-//   setCoodrinates,
-//   position,
-//   axis,
-// }: {
-//   errors: ErrorType;
-//   areaCoodrinate: AreaCoordinateType | undefined;
-//   setCoodrinates: (target: any, index: number, axis: string) => void;
-//   position: number;
-//   axis: string;
-// }) => {
-//   return (
-//     <div className={errors.y1 && 'input-error'}>
-//       <Label label="Y-coordinate" />
-//       <Input
-//         value={
-//           axis === 'x'
-//             ? areaCoodrinate?.coordinates[position].x
-//             : areaCoodrinate?.coordinates[position].y
-//         }
-//         onChange={(input: { target: any }) =>
-//           setCoodrinates(input.target, position, axis)
-//         }
-//       />
-//       {errors?.y1}
-//     </div>
-//   );
-// };
