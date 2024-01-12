d186dc8d4f8b63eae223e6a37250ee074728610e
mheggelund <maohe@equinor.com>
2024-01-12 08:48:36 +0100

    chore: update generated api files.

---
+++ src/api/generated/models/UpdateComputeCaseCommandForm.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { UpdateComputeCaseInputSettingsForm } from './UpdateComputeCaseInputSettingsForm';
+
+export type UpdateComputeCaseCommandForm = {
+    modelAreaId?: string | null;
+    inputSettings: Array<UpdateComputeCaseInputSettingsForm>;
+};
+
