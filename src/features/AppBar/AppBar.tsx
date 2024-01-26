4eb05dfa86b1de14e0dfc629d5cd773486443320
mheggelund <maohe@equinor.com>
2024-01-26 14:11:54 +0100

    testing for 404 error

--- src/features/AppBar/AppBar.tsx
+++ src/features/AppBar/AppBar.tsx
@@ -2,7 +2,7 @@ import { TopBar } from '@equinor/eds-core-react';
 import { useNavigate } from 'react-router-dom';
 import * as Styled from './AppBar.styled';
 import { Icons } from './Icons/Icons';
-import { Navigation } from './Navigation/Navigation';
+// import { Navigation } from './Navigation/Navigation';
 
 const AppBar = ({ title }: { title: string }) => {
   const navigate = useNavigate();
@@ -16,7 +16,7 @@ const AppBar = ({ title }: { title: string }) => {
       >
         {title}
       </Styled.Header>
-      <Navigation />
+      {/* <Navigation /> */}
       <TopBar.Actions>
         <Icons />
       </TopBar.Actions>
