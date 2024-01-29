3b2ba1c3eda9d4c9eb0248d6f7c2e1acfa8060d5
mheggelund <maohe@equinor.com>
2024-01-29 16:24:20 +0100

    fix: remove logging for debugging.

--- src/features/AppBar/AppBar.tsx
+++ src/features/AppBar/AppBar.tsx
@@ -1,13 +1,11 @@
 import { TopBar } from '@equinor/eds-core-react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import * as Styled from './AppBar.styled';
 import { Icons } from './Icons/Icons';
 import { Navigation } from './Navigation/Navigation';
 
 const AppBar = ({ title }: { title: string }) => {
   const navigate = useNavigate();
-  const location = useLocation();
-  console.log(location.pathname);
 
   return (
     <Styled.TopBar>
