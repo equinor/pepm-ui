dcc5a4759bb216181bd00522270b00a1999067ac
Mathias Oppedal Heggelund <98742460+mheggelund@users.noreply.github.com>
2024-01-31 13:45:08 +0100

    fix: test rewrite router (#219)

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, NonIndexRouteObject } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { ModelView } from './features/ModelView/ModelView';
 import { About } from './pages/About/About';
@@ -11,64 +11,52 @@ import { Model } from './pages/ModelPages/Model/Model';
 import { ObjectResult } from './pages/ModelPages/Results/ObjectResult/ObjectResult';
 import { VariogramResults } from './pages/ModelPages/Results/VariogramResults/VariogramResults';
 
-interface Tab extends Required<Pick<NonIndexRouteObject, 'path' | 'element'>> {
-  title: string;
-}
-
-const tabs: Tab[] = [
-  { title: 'Models', path: '/', element: <Browse /> },
-  { title: 'API', path: 'api', element: <Api /> },
-  { title: 'About', path: 'about', element: <About /> },
-];
-
-const appRoutes = [
-  {
-    index: true,
-    element: <Browse />,
-  },
-  {
-    path: 'api',
-    element: <Api />,
-  },
-  {
-    path: 'about',
-    element: <About />,
-  },
-  {
-    path: ':modelId/',
-    element: <Model />,
-    children: [
-      {
-        path: 'details',
-        element: <ModelView />,
-      },
-
-      {
-        path: 'compute/variogram',
-        element: <ComputeVariogram />,
-      },
-      {
-        path: 'compute/object',
-        element: <ComputeObject />,
-      },
-
-      {
-        path: 'results/variogram',
-        element: <VariogramResults />,
-      },
-      {
-        path: 'results/object',
-        element: <ObjectResult />,
-      },
-    ],
-  },
-];
-
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: appRoutes,
+    children: [
+      {
+        index: true,
+        element: <Browse />,
+      },
+      {
+        path: 'api',
+        element: <Api />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: ':modelId/',
+        element: <Model />,
+        children: [
+          {
+            path: 'details',
+            element: <ModelView />,
+          },
+
+          {
+            path: 'compute/variogram',
+            element: <ComputeVariogram />,
+          },
+          {
+            path: 'compute/object',
+            element: <ComputeObject />,
+          },
+
+          {
+            path: 'results/variogram',
+            element: <VariogramResults />,
+          },
+          {
+            path: 'results/object',
+            element: <ObjectResult />,
+          },
+        ],
+      },
+    ],
   },
   {
     path: '*',
@@ -76,4 +64,4 @@ const router = createBrowserRouter([
   },
 ]);
 
-export { router, tabs };
+export { router };
