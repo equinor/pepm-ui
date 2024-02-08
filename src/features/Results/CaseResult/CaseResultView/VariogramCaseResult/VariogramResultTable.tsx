6b39a7a1b887b4d8ce81972feea73d58446f9607
mheggelund <maohe@equinor.com>
2024-02-08 13:43:13 +0100

    chore: set som datafields with real data.

--- src/features/Results/CaseResult/CaseResultView/VariogramCaseResult/VariogramResultTable.tsx
+++ src/features/Results/CaseResult/CaseResultView/VariogramCaseResult/VariogramResultTable.tsx
@@ -19,19 +19,19 @@ export const VariogramResultTable = ({
         </Table.Row>
         <Table.Row>
           <Table.Cell>Compute method</Table.Cell>
-          <Table.Cell>--Data--</Table.Cell>
+          <Table.Cell>--Method--</Table.Cell>
         </Table.Row>
         <Table.Row>
           <Table.Cell>Attribute</Table.Cell>
-          <Table.Cell>--Data--</Table.Cell>
+          <Table.Cell>{data.attribute ? data.attribute : '--'}</Table.Cell>
         </Table.Row>
         <Table.Row>
           <Table.Cell>Variogram model</Table.Cell>
-          <Table.Cell>--Data--</Table.Cell>
+          <Table.Cell>{data.family}</Table.Cell>
         </Table.Row>
         <Table.Row>
           <Table.Cell>Sigma</Table.Cell>
-          <Table.Cell>--Data--</Table.Cell>
+          <Table.Cell>{data.sigma}</Table.Cell>
         </Table.Row>
       </Table.Body>
     </Table>
