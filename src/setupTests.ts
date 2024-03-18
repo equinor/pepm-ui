341da813225593538adb2020506e955b77ad7f27
mheggelund <maohe@equinor.com>
2024-03-18 09:11:14 +0100

    test: Setup for running tests.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,18 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+global.ResizeObserver = class ResizeObserver {
+  observe() {
+    // You can fill in the details of what you want observe to do
+  }
+  unobserve() {
+    // You can fill in the details of what you want unobserve to do
+  }
+  disconnect() {
+    // You can fill in the details of what you want disconnect to do
+  }
+};
+
 jest.mock('./auth/msalClient.ts', () => ({
   getActiveAccount: () => ({}),
   aquireTokenSilent: () => Promise.resolve({ accessToken: '' }),
