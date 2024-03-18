bee55aaf813540ec9f926d87dc78046210ac2180
mheggelund <maohe@equinor.com>
2024-03-18 15:14:10 +0100

    refactor: Revrite hook to simplify and be able to test.

--- src/components/AreaCoordinates/hooks/useModelResults.tsx
+++ src/components/AreaCoordinates/hooks/useModelResults.tsx
@@ -1,11 +1,12 @@
-import { useFetchCases } from '../../../hooks/useFetchCases';
+import { ComputeCaseDto } from '../../../api/generated';
 
-export const useModelResults = (activeArea: string) => {
-  const cases = useFetchCases();
-
+export const useModelResults = (
+  activeArea: string,
+  cases: ComputeCaseDto[] | undefined,
+) => {
   const activeAreaResultList =
-    cases.data &&
-    cases.data.data
+    cases &&
+    cases
       .filter((c) => c.modelArea !== null)
       .filter((ca) => ca.modelArea.name === activeArea);
 
