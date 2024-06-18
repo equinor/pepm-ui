139b1492b35c4ba182e5d72cb4983c5a3101c7f3
Wilhelm Vold <wvo@equinor.com>
2024-06-18 09:33:58 +0200

    fix: updated naming

--- src/hooks/useFetchChannelResults.ts
+++ src/hooks/useFetchChannelResults.ts
@@ -5,13 +5,13 @@ import { useMsal } from '@azure/msal-react';
 import { useParams } from 'react-router-dom';
 import { useAccessToken } from './useAccessToken';
 
-export const useFetchChannelResults = () => {
+export const useFetchObjectResults = () => {
   const { modelId } = useParams();
   const { instance, accounts } = useMsal();
   const token = useAccessToken(instance, accounts[0]);
 
   const query = useQuery({
-    queryKey: ['channel-results', modelId],
+    queryKey: ['object-results', modelId],
     queryFn: () =>
       ResultsService.getApiAnalogueModelsResultsObject(modelId as string),
     enabled: !!token,
--- src/pages/ModelPages/Results/ObjectResult/ObjectResult.tsx
+++ src/pages/ModelPages/Results/ObjectResult/ObjectResult.tsx
@@ -1,11 +1,11 @@
 import { CaseResultView } from '../../../../features/Results/CaseResult/CaseResultView/CaseResultView';
 import { NoResults } from '../../../../features/Results/NoResults/NoResults';
 import { useFetchCases } from '../../../../hooks/useFetchCases';
-import { useFetchChannelResults } from '../../../../hooks/useFetchChannelResults';
+import { useFetchObjectResults } from '../../../../hooks/useFetchChannelResults';
 
 export const ObjectResult = () => {
   const cases = useFetchCases();
-  const { data, isLoading } = useFetchChannelResults();
+  const { data, isLoading } = useFetchObjectResults();
   const objectResults = data?.data;
 
   if (isLoading) return <>Loading ...</>;
