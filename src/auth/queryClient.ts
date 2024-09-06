189811214ae44c72c4a3f6391df2ac5bd2e92ac0
mheggelund <maohe@equinor.com>
2024-09-06 14:36:45 +0200

    chore: Revert tanstack/react-querry back to v4. Fix errer EDS introduse with popocer failing tests. Comment out failing MenuIcon test.

--- src/auth/queryClient.ts
+++ src/auth/queryClient.ts
@@ -4,6 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
     },
   },
 });
