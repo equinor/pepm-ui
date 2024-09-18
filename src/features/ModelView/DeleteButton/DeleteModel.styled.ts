112e3e32e342c8f0b14eb22804de746564ea3758
Ørjan Aare Jørgensen <oajo@equinor.com>
2024-09-18 13:54:47 +0200

    chore: tune delete button and dialog

--- src/features/ModelView/DeleteButton/DeleteModel.styled.ts
+++ src/features/ModelView/DeleteButton/DeleteModel.styled.ts
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 import { spacings } from '../../../tokens/spacings';
+import { Dialog } from '@equinor/eds-core-react';
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: ${spacings.MEDIUM};
+  align-items: flex-start;
+`;
 
-  > button {
-    width: fit-content;
+export const DeleteDialog = styled(Dialog)`
+  min-width: 336px;
+
+  .actions {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: ${spacings.SMALL};
   }
 `;
