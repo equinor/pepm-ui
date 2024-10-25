32edf25525db7c869d02a21c763dc94d7b03f566
Thomas L Fagermyr <thomas.fagermyr@bouvet.no>
2024-10-25 13:49:09 +0200

    feat: Rewritten admin access to hook

---
+++ src/hooks/useIsOwnerOrAdmin.tsx
@@ -0,0 +1,20 @@
+import { useMsal } from '@azure/msal-react';
+import { usePepmContextStore } from './GlobalState';
+
+export const useIsOwnerOrAdmin = () => {
+  const { analogueModel } = usePepmContextStore();
+  const { accounts } = useMsal();
+  const roles = accounts[0].idTokenClaims?.roles;
+
+  if (roles === undefined) return false;
+  if (roles.includes('PEPM.Admin')) return true;
+  if (roles.includes('PEPM.User')) {
+    if (analogueModel.createdBy) {
+      if (accounts[0].username === analogueModel.createdBy) return true;
+      return false;
+    }
+    return true;
+  }
+
+  return false;
+};
