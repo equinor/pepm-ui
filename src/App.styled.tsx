f56c5aa0822b371923bbd837b8cbefa00a38a799
Thomas L Fagermyr <thomas.fagermyr@bouvet.no>
2024-11-26 10:08:52 +0100

    fix: Fixed footer overlapping content

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 
 const StyledOutletWrapper = styled.section`
   height: calc(100% - 128px);
+  overflow: auto;
 `;
+
 export { StyledOutletWrapper as OutletWrapper };
