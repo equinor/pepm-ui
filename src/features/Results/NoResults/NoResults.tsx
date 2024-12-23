a450939526aa150ac06c47b56879836525866622
Ørjan Aare Jørgensen <oajo@equinor.com>
2024-12-23 10:07:02 +0100

    fix: Clean up no-results page

--- src/features/Results/NoResults/NoResults.tsx
+++ src/features/Results/NoResults/NoResults.tsx
@@ -1,29 +1,18 @@
 import { Typography } from '@equinor/eds-core-react';
-import { InfoPageComponent } from '../../../components/InfoPageComponent/InfoPageComponent';
-import NoResultPicture from './NoResults.jpeg';
 import * as Styled from './NoResults.styled';
+import { Link } from 'react-router-dom';
 
 export const NoResults = () => {
   return (
-    <InfoPageComponent scaleHight="true">
-      <Styled.Img src={NoResultPicture} alt="altText" />
-      <Styled.TextDiv>
-        <Typography variant="h4">
-          Oh no! It seems we don’t have any results for this model yet
-        </Typography>
-        <Typography variant="body_long">
-          To get started, try adding cases to the <span>&nbsp;</span>
-          <Typography link href={'../compute/variogram'}>
-            variogram
-          </Typography>
-          <span>&nbsp;</span>
-          or <span>&nbsp;</span>
-          <Typography link href={'../compute/object'}>
-            object
-          </Typography>
-          <span>&nbsp;</span> page.
-        </Typography>
-      </Styled.TextDiv>
-    </InfoPageComponent>
+    <Styled.NoResults>
+      <Typography variant="h2">No results found</Typography>
+      <Typography variant="body_long">
+        Please check your compute settings for
+        <Link to={'../compute/variogram'}>variogram</Link>
+        or
+        <Link to={'../compute/object'}>object</Link>
+        cases.
+      </Typography>
+    </Styled.NoResults>
   );
 };
