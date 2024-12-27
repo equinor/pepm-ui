2dfba0c7f6dbb78db1b29902b8a64ddad2ef62d3
Ørjan Aare Jørgensen <oajo@equinor.com>
2024-12-27 12:56:49 +0100

    fix: Add animated progress indicator to object- and variogram result pages

--- src/pages/ModelPages/Results/VariogramResults/VariogramResults.tsx
+++ src/pages/ModelPages/Results/VariogramResults/VariogramResults.tsx
@@ -3,6 +3,8 @@ import { CaseResultView } from '../../../../features/Results/CaseResult/CaseResu
 import { NoResults } from '../../../../features/Results/NoResults/NoResults';
 import { usePepmContextStore } from '../../../../hooks/GlobalState';
 import { useFetchVariogramResults } from '../../../../hooks/useFetchVariogramResults';
+import { CircularProgress, Typography } from '@equinor/eds-core-react';
+import * as Styled from '../../Model/Model.styled';
 
 export const VariogramResults = () => {
   const { data, isLoading } = useFetchVariogramResults();
@@ -13,7 +15,20 @@ export const VariogramResults = () => {
     if (data) setVariogramResults(data.data);
   }, [data, setVariogramResults]);
 
-  if (isLoading) return <>Loading ...</>;
+  if (isLoading)
+    return (
+      <Styled.EmptyPage>
+        <div className="loading">
+          <CircularProgress
+            color="primary"
+            size={24}
+            value={100}
+            variant="indeterminate"
+          />
+          <Typography variant="body_short">Loading, please wait…</Typography>
+        </div>
+      </Styled.EmptyPage>
+    );
 
   return (
     <>
