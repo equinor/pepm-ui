e687d98a1a1b476bfad48c1347c7fc1b3393d8b6
Ørjan Aare Jørgensen <oajo@equinor.com>
2025-01-09 08:48:13 +0100

    fix: Smart links to variogram and object compute settings

--- src/features/Results/NoResults/NoResults.tsx
+++ src/features/Results/NoResults/NoResults.tsx
@@ -1,17 +1,22 @@
 import { Typography } from '@equinor/eds-core-react';
 import * as Styled from './NoResults.styled';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const NoResults = () => {
+  const location = useLocation();
+
   return (
     <Styled.NoResults>
       <Typography variant="h2">No results found</Typography>
       <Typography variant="body_long">
         Please check your compute settings for
-        <Link to={'../compute/variogram'}>variogram</Link>
-        or
-        <Link to={'../compute/object'}>object</Link>
-        cases.
+        {location.pathname.includes('variogram') && (
+          <Link to={'../compute/variogram'}>variogram</Link>
+        )}
+        {location.pathname.includes('object') && (
+          <Link to={'../compute/object'}>object</Link>
+        )}
+        case.
       </Typography>
     </Styled.NoResults>
   );
