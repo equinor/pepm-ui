3dea51a38b255e97033f82d8c438d398b255357c
Ørjan Aare Jørgensen <oajo@equinor.com>
2025-01-24 09:58:41 +0100

    fix: Align tables in object results (#424)

--- src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ChannelResult.styled.ts
+++ src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ChannelResult.styled.ts
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
-import { spacings } from '../../../../../tokens/spacings';
 import { theme } from '../../../../../tokens/theme';
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  column-gap: ${spacings.LARGE};
-  border: solid 0.5px ${theme.light.ui.background.medium};
+  display: grid;
+  grid-template-columns: subgrid;
+  border: solid 1px ${theme.light.ui.background.medium};
 `;
--- src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ChannelResultTable/ChannelResultTable.styled.tsx
+++ src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ChannelResultTable/ChannelResultTable.styled.tsx
@@ -4,35 +4,37 @@ import styled from 'styled-components';
 import { theme } from '../../../../../../tokens/theme';
 
 const StyledTable = styled(Table)`
-  width: 100%;
-  max-width: 800px;
+  border-collapse: collapse;
+  font-variant-numeric: tabular-nums;
+
+  tr:last-child td {
+    border-bottom: 0;
+  }
 `;
+
 export const ColumnCell = styled(Table.Cell)`
   background: ${theme.light.ui.background.light};
-  border: solid 0.5px ${theme.light.ui.background.medium};
+  font-weight: 700;
+  border-right: 1px solid ${theme.light.ui.background.medium};
   white-space: nowrap;
 `;
 
 export const DataCell = styled(Table.Cell)`
   text-align: right;
-  border: solid 0.5px ${theme.light.ui.background.medium};
-  width: 150px;
   white-space: nowrap;
 
+  &:not(:last-child) {
+    border-right: 1px solid ${theme.light.ui.background.medium};
+  }
+
   > div {
-    display: flex;
     justify-content: right;
   }
 `;
 
 export const InfoCell = styled(Table.Cell)`
   text-align: right;
-  border: solid 0.5px ${theme.light.ui.background.medium};
-  width: 20%;
-  > div {
-    display: flex;
-    justify-content: right;
-  }
+  border-right: 1px solid ${theme.light.ui.background.medium};
 `;
 
 export { StyledTable as Table };
