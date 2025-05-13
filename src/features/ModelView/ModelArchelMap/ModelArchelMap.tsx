2d54fd9e5484ab53e9e578218e5fbbde1ed178b6
Wilhelm Vold <43452613+Sinrefvol@users.noreply.github.com>
2025-05-13 08:42:22 +0200

    fix: added conditional edit button rendering, removed rendering of empty table (#480)

--- src/features/ModelView/ModelArchelMap/ModelArchelMap.tsx
+++ src/features/ModelView/ModelArchelMap/ModelArchelMap.tsx
@@ -2,13 +2,19 @@ import { Typography } from '@equinor/eds-core-react';
 import { ModelArchelMapDialog } from './components/ModelArchelMapDialog';
 import { ModelArchelMapTable } from './components/ModelArchelMapTable';
 import { ArchelMapContainer } from './ModelArchelMap.styled';
+import { usePepmContextStore } from '../../../stores/GlobalStore';
 export const ModelArchelMap = () => {
+  const { analogueModel } = usePepmContextStore();
   return (
     <ArchelMapContainer>
       <Typography variant="h4" as="h3">
         Architectural Elements
       </Typography>
-      <ModelArchelMapTable />
+      {analogueModel.iniParameters.archels
+        .filter((a) => a.analogueModelComputeSettingArchelMap !== null)
+        .map((a) => a.analogueModelComputeSettingArchelMap).length !== 0 && (
+        <ModelArchelMapTable />
+      )}
       <ModelArchelMapDialog />
     </ArchelMapContainer>
   );
