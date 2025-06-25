d2344d2dc57ca272357e59bca3c87cb8d4a82639
Wilhelm Vold <43452613+Sinrefvol@users.noreply.github.com>
2025-06-25 09:53:49 +0200

    fix: added failsafe if archels dont exist on the model (#488)

--- src/features/ModelView/ModelArchelMap/ModelArchelMap.tsx
+++ src/features/ModelView/ModelArchelMap/ModelArchelMap.tsx
@@ -2,16 +2,21 @@ import { ModelArchelMapDialog } from './components/ModelArchelMapDialog';
 import { ModelArchelMapTable } from './components/ModelArchelMapTable';
 import { ArchelMapContainer } from './ModelArchelMap.styled';
 import { usePepmContextStore } from '../../../stores/GlobalStore';
+import { Typography } from '@equinor/eds-core-react';
 export const ModelArchelMap = () => {
   const { analogueModel } = usePepmContextStore();
-  return (
+  return analogueModel.iniParameters?.archels ? (
     <ArchelMapContainer>
-      {analogueModel.iniParameters.archels
+      {analogueModel.iniParameters?.archels
         .filter((a) => a.analogueModelComputeSettingArchelMap !== null)
         .map((a) => a.analogueModelComputeSettingArchelMap).length !== 0 && (
         <ModelArchelMapTable />
       )}
       <ModelArchelMapDialog />
     </ArchelMapContainer>
+  ) : (
+    <>
+      <Typography>Could not retrieve archels from this model</Typography>
+    </>
   );
 };
