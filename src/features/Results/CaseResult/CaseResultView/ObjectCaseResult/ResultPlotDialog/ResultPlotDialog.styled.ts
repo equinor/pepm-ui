6c6bcdeab11535c198346333b0f6b445dee8a4fb
Thomas L Fagermyr <thomas.fagermyr@bouvet.no>
2026-02-17 11:05:01 +0100

    feat: Added some changes after user feedback

--- src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ResultPlotDialog/ResultPlotDialog.styled.ts
+++ src/features/Results/CaseResult/CaseResultView/ObjectCaseResult/ResultPlotDialog/ResultPlotDialog.styled.ts
@@ -6,11 +6,12 @@ export const Content = styled(Dialog.Content)`
   display: flex;
   flex-direction: column;
   row-gap: ${spacings.MEDIUM};
+  width: 100%;
 `;
 
 export const GraphDialog = styled(Dialog)`
   min-height: 400px;
-  min-width: 700px;
+  width: 700px;
 `;
 
 export const RadioGroup = styled.div`
