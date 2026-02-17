1bf217f3bbedffb1766a028f98559902592ad864
Wilhelm Vold <43452613+Sinrefvol@users.noreply.github.com>
2026-02-17 13:27:20 +0100

    feat: added extra filtering on tables and added toggle columns to model table (#521)

--- src/features/ModelTable/ModelTable.styled.ts
+++ src/features/ModelTable/ModelTable.styled.ts
@@ -1,8 +1,27 @@
 import styled from 'styled-components';
+import { SideSheet, Typography } from '@equinor/eds-core-react';
 import { spacings } from '../../tokens/spacings';
 import { theme } from '../../tokens/theme';
 
+export const SideSheetContent = styled.div`
+  padding: ${spacings.MEDIUM};
+`;
+
+export const SideSheetTitle = styled(Typography)`
+  display: block;
+  margin-bottom: ${spacings.SMALL};
+  font-weight: bold;
+`;
+
+export const CheckboxColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
   .table-wrapper {
     border: 1px solid ${theme.light.ui.background.medium};
     max-height: calc(
@@ -20,6 +39,26 @@ export const Buttons = styled.div`
   flex-direction: row;
   justify-content: center;
   column-gap: ${spacings.SMALL};
+`;
+
+export const ColumnsButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding-bottom: ${spacings.MEDIUM};
+  gap: ${spacings.X_SMALL};
+`;
+
+export const StyledSideSheet = styled(SideSheet)`
+  top: ${spacings.XXXX_LARGE} !important;
+  bottom: ${spacings.XXXX_LARGE} !important;
+  height: calc(
+    100dvh - ${spacings.XXXX_LARGE} - ${spacings.XXXX_LARGE}
+  ) !important;
+  max-height: calc(
+    100dvh - ${spacings.XXXX_LARGE} - ${spacings.XXXX_LARGE}
+  ) !important;
+  overflow-y: auto !important;
 `;
 
 export const List = styled.div`
